d6e3698d0ac65d8fb0be6014275943a524c4d2f6
Daniel Kristiyanto <danielkr@uw.edu>
2016-03-22 19:44:27 -0700

    update itraq

--- itraq.R
+++ itraq.R
@@ -1,17 +1,35 @@
 library(MSnbase)
 library(mzID)
 
-setwd("~/Documents/GITHUB/msgf/data/")
+setwd("C:/Users/kris239/Desktop/itraq")
+
+####################################### READ FILE ###################################################
 
 mzid.files        <- list.files(path = ".", pattern ="mzid", all.files = F, 
-                       full.names = F, recursive = F, ignore.case = T, include.dirs = F)
+                       full.names = F, recursive = F, ignore.case = T, include.dirs = F)[2]
 mzml.files        <- list.files(path = ".", pattern ="mzML$", all.files = F, 
-                       full.names = F, recursive = F, ignore.case = T, include.dirs = F)
-mzids.raw         <- mzID(mzid.files)
-msexp.raw         <- readMSData(mzml.files, verbose = FALSE)
-msexp.id          <- addIdentificationData(object=msexp.raw, id = mzid.files, verbose=T)
-idSummary(msexp.id)
-msexp.id
+                       full.names = F, recursive = F, ignore.case = T, include.dirs = F)[2]
 
-head(fData(msexp.id))
-qnt <- quantify(msexp.id, method="trap", reporters=iTRAQ4, strict=F)
+mzids.raw         <- mzID(mzid.files)
+msexp.raw         <- readMSData(mzml.files)
+
+####################################### IDENTIFICATION ###################################################
+
+msexp             <- addIdentificationData(msexp.raw, id = mzids.raw)
+msexp             <- removeNoId(msexp)
+msexp             <- removeMultipleAssignment(msexp)
+idSummary(msexp)
+
+####################################### QUANTIFICATION ###################################################
+qnt               <- quantify(msexp, method="max", reporters=iTRAQ4, strict=F, verbose=F)
+qnt               <- filterNA(qnt, pNA = 0)
+agg               <- combineFeatures(qnt, groupBy = fData(qnt.filtered)$accession, fun="mean")
+
+####################################### CLEAN UP ###################################################
+rm(mzid.files)
+rm(mzml.files)
+
+####################################### OUTPUT ###################################################
+save.image(file="Labelled-Quant.RData")
+write.csv(cbind(Assccession=row.names(agg),exprs(agg)), row.names = F)
+
